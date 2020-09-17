c013b0ae442e54469230c77903b3914bd74db8b8
Imandana Rahimaswara <41320608+imandana@users.noreply.github.com>
2020-09-17 20:42:58 +0700

    Update main.cpp

--- ImandanaPlayer/ImandanaPlayer/main.cpp
+++ ImandanaPlayer/ImandanaPlayer/main.cpp
@@ -3,7 +3,6 @@
 #include <mono/metadata/environment.h>
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/debug-helpers.h>
-//#include <glib.h>
 #include <string.h>
 #include <stdlib.h>
 #include <vector>
@@ -12,17 +11,6 @@
 #define FALSE 0
 #endif
 
-/*
- * Simple mono embedding example.
- * We show how to create objects and invoke methods and set fields in them.
- * Compile with: 
- * 	gcc -Wall -o test-invoke test-invoke.c `pkg-config --cflags --libs mono-2` -lm
- * 	mcs invoke.cs
- * Run with:
- * 	./test-invoke invoke.exe
- */
-
-/* Demostrate how to call methods */
 static void
 call_methods (MonoObject *obj, char* pMethodName)
 {
@@ -45,15 +33,8 @@ call_methods (MonoObject *obj, char* pMethodName)
 			method = m;
 		} 
 	}
-	/* Now we'll call method () on obj: since it takes no arguments 
-	 * we can pass NULL as the third argument to mono_runtime_invoke ().
-	 * The method will print the updated value.
-	 */
-
 	if(isThere)
 	mono_runtime_invoke (method, obj, NULL, NULL);
-	//mono_runtime_invoke (fail, obj, NULL, NULL);
-
 }
 
 static void
@@ -78,10 +59,6 @@ static void
 		class_list.push_back(_class);
 	}
 	uint32_t type_token=0;
-
-	//std::vector<MonoObject *> objs;
-
-	///klass = mono_class_from_name (image, "", "ScriptMove");
 	klass = mono_class_from_name (image, "PersatuanEngine", "MiniBehaviour");
 	printf ( "Nama Classnya %s\n", mono_class_get_name (klass));
 
@@ -98,7 +75,6 @@ static void
 				objs->push_back(mono_object_new (domain, class_list[i]));
 	}
 	//obj = mono_object_new (domain, klass);
-		
 	for(int i =0; i < objs->size(); i++)
 	{
 				mono_runtime_object_init ((*objs)[i]);
@@ -109,19 +85,10 @@ static void
 static void main_function (MonoDomain *domain, const char *file, int argc, char **argv)
 {
 	MonoAssembly *assembly;
-
-	/* Loading an assembly makes the runtime setup everything
-	 * needed to execute it. If we're just interested in the metadata
-	 * we'd use mono_image_load (), instead and we'd get a MonoImage*.
-	 */
 	assembly = mono_domain_assembly_open (domain, file);
 	if (!assembly)
 		exit (2);
-	/*
-	 * mono_jit_exec() will run the Main() method in the assembly.
-	 * The return value needs to be looked up from
-	 * System.Environment.ExitCode.
-	 */
+
 	mono_jit_exec (domain, assembly, argc, argv);
 
 	std::vector<MonoObject*> theObjs;
@@ -139,16 +106,9 @@ main (int argc, char* argv[]) {
 	MonoDomain *domain;
 	const char *file;
 	int retval;
-	
-	/*if (argc < 2){
-		fprintf (stderr, "Please provide an assembly to load\n");
-		return 1;
-	}*/
+
 	file = "AssemblyCSharp.dll"; //argv [1];
-	/*
-	 * mono_jit_init() creates a domain: each assembly is
-	 * loaded and run in a MonoDomain.
-	 */
+
 	domain = mono_jit_init (file);
 
 	main_function (domain, file, argc - 1, argv + 1);
